9e606162910657e4c13333c57e14a2998b64f65d
raval <ravalbb@gmail.com>
2017-04-21 15:01:48 +0200

    add directories and files

--- src/main/java/Searcher.java
+++ src/main/java/Searcher.java
@@ -1,7 +1,11 @@
 package main.java;
 
+import java.io.File;
+
 public class Searcher {
     public static void main(String[] args) {
+
+        File directoryContent = new File("./search_dir");
 
     }
 }
